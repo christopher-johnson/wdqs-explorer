c3f175841facfa74f66e5b09603c58298be84f3c
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-01-26 23:42:33 +0100

    adds GET parameter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var $ =  require('jquery');
+var url = require('wurl');
 
 var wdqsExplorer = require('./wdqsExplorer.js');
 var Explorer = require('./Explorer.js');
@@ -8,7 +9,11 @@ function getRandomId(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var item = 'https://www.wikidata.org/entity/Q' + getRandomId(1,18000000);
+if (url('?id')) {
+    var item = 'https://www.wikidata.org/entity/' + url('?id');
+} else {
+    item = 'https://www.wikidata.org/entity/Q' + getRandomId(1, 18000000);
+}
 
 $( document ).ready( function () {
     new Explorer( wdqsExplorer, item );
